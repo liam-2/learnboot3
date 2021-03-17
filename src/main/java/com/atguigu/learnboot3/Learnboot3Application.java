0a1638b0919d35b39a89f16818646867705d3333
liam <liam@easestrategy.com>
2021-03-17 18:07:33 +0800

    学到p44

---
+++ src/main/java/com/atguigu/learnboot3/Learnboot3Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.learnboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learnboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learnboot3Application.class, args);
+    }
+
+}
